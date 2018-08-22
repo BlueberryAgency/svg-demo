3bc8829b15d1afbfc325b38a3dc69c66b34e7efb
James Kent <jkent@blueengine.com.au>
2018-08-22 21:18:26 +1000

    With animated SVG section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,21 @@ import { SvgLoader, SvgProxy } from "react-svgmt";
 const svgUrl = "/product.svg";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { toggle: false };
+  }
+
+  componentDidMount() {
+    var that = this;
+    setInterval(() => {
+      that.setState({ toggle: !that.state.toggle });
+    }, 1000);
+ }
+
   render() {
+    console.log(this.state.toggle);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -18,7 +32,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
           <div style={{ width: 400 }}>
             <SvgLoader path={svgUrl}>
-              <SvgProxy selector="rect.custom_front-top-left-outline" stroke-opacity="1" onclick={() => console.log('click')} />
+              <SvgProxy selector="rect.custom_front-top-left-outline" stroke-opacity={ this.state.toggle ? "1" : "0" } onclick={() => console.log('click')} />
             </SvgLoader>
           </div>
         </p>
